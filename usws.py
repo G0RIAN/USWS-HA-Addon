1ff300cfaf90ed5303522555b33e656bd2c08d83
G0rian <moritzweixler@outlook.de>
2022-05-17 20:06:22 +0200

    fix: add exceptions for html parsing

--- usws.py
+++ usws.py
@@ -13,22 +13,7 @@ class USWS(hass.Hass):
     url = "https://lhg-902.iws.uni-stuttgart.de/"
     pool_manager = urllib3.PoolManager()
 
-    # weather state attributes
-    temp = None             # Air temperature
-    temp_app = None         # Apparent air temperature
-    precip_hourly = None    # Precipitation in the last hour
-    precip_daily = None     # Precipitation daily sum
-    press = None            # Air pressure MSL
-    humid_rel = None        # Relative humidity
-    vap_press = None        # Vapour pressure
-    dew_pt = None           # Dew point
-    rad_short_glob = None   # Global short-wave radiation
-    rad_short_ref = None    # Reflected short-wave radiation
-    rad_long_atm = None     # Atmospheric long-wave radiation
-    rad_long_terr = None    # Terrestrial long-wave radiation
-    wind_dir = None         # Wind direction
-    wind_speed = None       # Wind speed
-    wind_speed_sq = None    # Wind speed (squall)
+    sensor_data = dict()
 
     def initialize(self):
         self.request_time_sec = int(self.args.get("request_time_sec", self.request_time_sec))
@@ -47,39 +32,38 @@ class USWS(hass.Hass):
         self.update_entity()
 
     def parse_weather_data(self, soup):
-        self.temp = float(soup.find("span", {"id": "T_c_1"}).text)
-        self.temp_app = float(soup.find("span", {"id": "T_f_1"}).text)
-        self.precip_hourly = float(soup.find("span", {"id": "N_1h_1"}).text)
-        self.precip_daily = float(soup.find("span", {"id": "N_24h_d"}).text)
-        self.press = float(soup.find("span", {"id": "P_c"}).text)
-        self.humid_rel = int(soup.find("span", {"id": "rh_c_1"}).text)
-        self.vap_press = float(soup.find("span", {"id": "ed_c"}).text)
-        self.dew_pt = float(soup.find("span", {"id": "Td_c"}).text)
-        self.rad_short_glob = float(soup.find("span", {"id": "G_c"}).text)
-        self.rad_short_ref = float(soup.find("span", {"id": "RK_c"}).text)
-        self.rad_long_atm = float(soup.find("span", {"id": "A_c"}).text)
-        self.rad_long_terr = float(soup.find("span", {"id": "E_c"}).text)
-        self.wind_dir = float(soup.find("span", {"id": "dd_c"}).text)
-        self.wind_speed = 0.0  # TODO: get wind speed (maybe from image?)
-        self.wind_speed_sq = float(soup.find("span", {"id": "u_cmax"}).text)
+        self.sensor_data = dict()
+        span_ids = {
+           "temperature": "T_c_1",
+           "apparent temperature": "T_f_1",
+           "hourly precipitation": "N_1h_1",
+           "daily precipitation": "N_24h_d",
+           "pressure": "P_c",
+           "humidity": "rh_c_1",
+           "vapour pressure": "ed_c",
+           "dew point": "Td_c",
+           "global short-wave radiation": "G_c",
+           "reflected short-wave radiation": "RK_c",
+           "atmospheric long-wave radiation": "A_c",
+           "terrestrial long-wave radiation": "E_c",
+           "wind direction": "dd_c",
+           #  "wind speed": "???",
+           "wind speed squall": "u_cmax"
+        }
+
+        for key, span_id in span_ids:
+            try:
+                result = soup.find("span", {"id": span_id})
+                if result is not None:
+                    self.sensor_data[key] = float(result.text)
+            except Exception as e:
+                print(e)
+
+
+        self.sensor_data["wind speed"] = 0.0  # TODO: get wind speed (maybe from image?)
 
     def update_entity(self):
-        attribs = {
-            "temperature": self.temp,
-            "apparent temperature": self.temp_app,
-            "hourly precipitation": self.precip_hourly,
-            "daily precipitation": self.precip_daily,
-            "pressure": self.press,
-            "humidity": self.humid_rel,
-            "vapour pressure": self.vap_press,
-            "dew point": self.dew_pt,
-            "global short-wave radiation": self.rad_short_glob,
-            "reflected short-wave radiation": self.rad_short_ref,
-            "atmospheric long-wave radiation": self.rad_long_atm,
-            "terrestrial long-wave radiation": self.rad_long_terr,
-            "wind direction": self.wind_dir,
-            "wind speed": self.wind_speed,
-            "wind speed squall": self.wind_speed_sq
-        }
-        self.get_entity(self.sensor_name).set_state(self.temp, attribs)
+        attribs = self.sensor_data
+        attribs.pop("temperature")
+        self.get_entity(self.sensor_name).set_state(self.sensor_data["temperature"], attribs)
 
