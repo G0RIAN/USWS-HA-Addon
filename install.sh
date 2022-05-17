d4369505bb7109fbc5862599246b9ffd3bd85d5d
G0rian <moritzweixler@outlook.de>
2022-05-17 19:21:22 +0200

    feat: remove temp data after install
    remove temporary data (cloned repo) after installation

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@
 git clone git@github.com:G0RIAN/USWS-HA-Addon.git
 cp ./USWS-HA-Addon/usws.py /config/appdaemon/apps/
 cat ./USWS-HA-Addon/apps.yaml >> /config/appdaemon/apps/apps.yaml
+rm -r USWS-HA-Addon/
