b8258343248ba7af8455504ddb49cf44b7d38be9
G0rian <moritzweixler@outlook.de>
2022-05-17 21:11:16 +0200

    feature: switch to selenium
    - switch request from urllib3 to selenium webdriver
    - replace self.get_entity(name).set_state(...) method by self.set_state(name, ...) method

--- usws.py
+++ usws.py
@@ -3,7 +3,8 @@ import datetime
 
 import hassapi as hass
 from bs4 import BeautifulSoup
-import urllib3
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 class USWS(hass.Hass):
@@ -11,11 +12,20 @@ class USWS(hass.Hass):
     sensor_name = "sensor.usws"
     request_time_sec = random.Random().randint(0, 59)
     url = "https://lhg-902.iws.uni-stuttgart.de/"
-    pool_manager = urllib3.PoolManager()
-
+    driver = None
     sensor_data = dict()
 
     def initialize(self):
+        chrome_options = Options()
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--start-maximized")
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument("--window-size=1920,1080")
+        chrome_options.add_argument("--ignore-certificate-errors")
+        self.driver = webdriver.Chrome("chromedriver", options=chrome_options)
+
         self.request_time_sec = int(self.args.get("request_time_sec", self.request_time_sec))
         self.sensor_name = str(self.args.get("sensor_name", self.sensor_name))
         if self.sensor_name[0:7] != "sensor.":
@@ -25,9 +35,9 @@ class USWS(hass.Hass):
     def terminate(self):
         pass
 
-    def run_minutely_callback(self, kwargs):
-        response = self.pool_manager.request("GET", self.url)
-        soup = BeautifulSoup(response.data)
+    def run_minutely_callback(self, *args, **kwargs):
+        self.driver.get(self.url)
+        soup = BeautifulSoup(self.driver.page_source, "html.parser")
         self.parse_weather_data(soup)
         self.update_entity()
 
@@ -63,6 +73,4 @@ class USWS(hass.Hass):
 
     def update_entity(self):
         attribs = self.sensor_data
-        attribs.pop("temperature", None)
-        self.get_entity(self.sensor_name).set_state(self.sensor_data["temperature"], attribs)
-
+        self.set_state(self.sensor_name, state=attribs.pop("temperature", "unknown"), attributes=attribs)
